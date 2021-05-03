1dc3d0f687d5771d8306108fb5ec5609b8275639
JUNG-MIN WOO <jayxwoo@gmail.com>
2021-05-04 05:01:13 +1200

    nav onclick debugged

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,15 +10,12 @@ const Navbar = () => {
     const toggleMenu = () => {
         setMenuBtn(!menuBtn);
     };
-    const handleSignUpClick = (e) => {
-        console.log('Sign Up Clicked');
-        toggleMenu();
-    };
     const checkMobile = () => {
         if (window.innerWidth <= 850) {
             setIsMobile(true);
         } else {
             setIsMobile(false);
+            setMenuBtn(false);
         };
     };
     window.addEventListener('load', () => {
@@ -40,11 +37,11 @@ const Navbar = () => {
                 <LogoLink />
                 <nav className={menuBtn ? 'header__nav header__nav--active' : 'header__nav'}>
                     <ul className="header__nav-menu-group">
-                        <li className="header__nav-menu-item"><NavLink className="header__nav-menu-link" exact to="/" onClick={toggleMenu}>Home</NavLink></li>
-                        <li className="header__nav-menu-item"><NavLink className="header__nav-menu-link" to="/services" onClick={toggleMenu}>Services</NavLink></li>
-                        <li className="header__nav-menu-item"><NavLink className="header__nav-menu-link" to="/products" onClick={toggleMenu}>Products</NavLink></li>
+                        <li className="header__nav-menu-item"><NavLink className="header__nav-menu-link" exact to="/">Home</NavLink></li>
+                        <li className="header__nav-menu-item"><NavLink className="header__nav-menu-link" to="/services">Services</NavLink></li>
+                        <li className="header__nav-menu-item"><NavLink className="header__nav-menu-link" to="/products">Products</NavLink></li>
                     </ul>
-                    <Link to="/sign-up" className="sign-up-link"><Button onClick={(e) => handleSignUpClick(e)} btnStyle="btn-outline" btnSize={isMobile ? 'btn-large' : 'btn-medium'} className="btn-sign-up">Sign Up</Button></Link>
+                    <Link to="/sign-up" className="sign-up-link"><Button btnStyle="btn-outline" btnSize={isMobile ? 'btn-large' : 'btn-medium'} className="btn-sign-up">Sign Up</Button></Link>
                 </nav>
                 {isMobile && <button className="menu-btn" onClick={toggleMenu}><span className="material-icons menu-icon">{menuBtn ? 'close' : 'menu'}</span></button>}
             </div>
